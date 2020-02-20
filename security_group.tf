95404226109b8ae63ad0179a3f190568a389388b
henry Gallo <gallohenry@gmail.com>
2020-02-20 11:27:49 -0500

    added description to ingress of sec group

--- security_group.tf
+++ security_group.tf
@@ -4,6 +4,7 @@ resource "aws_security_group" "allow_http" {
   vpc_id      = var.vpc_id
 
   ingress {
+    description = var.sg_description
     from_port   = var.from_port
     to_port     = var.to_port
     protocol    = "tcp"
